cbc3d186b3645ba3a6a43c12690972b34d44d7b3
Christopher McIntyre <me@cgmcintyre.com>
2017-03-27 15:06:34 +0100

    Moved to gpg encryption for passwords

---
+++ mail/.offlineimap.py
@@ -0,0 +1,8 @@
+import subprocess
+import os
+
+BASE_PATH = os.path.join(os.path.expanduser('~'), '.passwords')
+
+def mailpasswd(account):
+    path = os.path.join(BASE_PATH, "mail-{}.gpg".format(account))
+    return subprocess.check_output(["gpg", "--quiet", "--use-agent", "-d", path]).strip()
